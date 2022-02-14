27f44187e357024a7c9b49675bdd1a5fc3d70c75
Janice Lee <meekukin@gmail.com>
2022-02-14 12:01:15 -0800

    created event handler for keydown

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,7 @@
+// var $letters = document.querySelectorAll('span');
+// var i = 0;
+// document.addEventListener('keydown', function(event) {
+// console.log(event.key);
+// if (event.key === $letters[i].textContent) {
+// }
+// })
