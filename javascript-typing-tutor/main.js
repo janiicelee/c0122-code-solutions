c9bc11da2b45bb38ec1d0569be39e415c31bd728
Janice Lee <meekukin@gmail.com>
2022-02-14 22:49:13 -0800

    Completed javascript-typing-tutor challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,12 @@
-// var $letters = document.querySelectorAll('span');
-// var i = 0;
-// document.addEventListener('keydown', function(event) {
-// console.log(event.key);
-// if (event.key === $letters[i].textContent) {
-// }
-// })
+var $letters = document.querySelectorAll('span');
+var i = 0;
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === $letters[i].textContent || event.keyCode === 32) {
+    $letters[i].className = 'right';
+    i++;
+    $letters[i].className = 'black-line';
+  } else {
+    $letters[i].className = 'wrong';
+  }
+});
