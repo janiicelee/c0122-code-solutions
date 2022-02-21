a172c382c58bf4474f4a6d4f7749fae3c6ff8454
Janice Lee <meekukin@gmail.com>
2022-02-20 21:39:01 -0800

    Completed array-manipulation-3 exercise

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,25 @@
 /* exported intersection */
+/*
+
+input: first array, second array
+output: array
+
+- create new storage for the output
+- only get the unique element in both arrays.
+  - if an element in the second array is not in the first array, don't add it to the new array
+  - only add the element that is also in the second array.
+- return the new array
+*/
+
+function intersection(first, second) {
+  var same = [];
+
+  for (var i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) === -1) {
+      continue;
+    }
+    same.push(first[i]);
+  }
+
+  return same;
+}
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,27 @@
 /* exported unique */
+/*
+
+input: array
+output: array
+
+- remove the duplicate elements in the order they first appear.
+- create a new storage => new array
+- loop over the given array
+- store it in the new array.
+- if the element in the given array is already added in the new array, don't add it.
+- return the new array
+
+*/
+
+function unique(array) {
+
+  var newArr = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (!newArr.includes(array[i])) {
+      newArr.push(array[i]);
+    }
+  }
+
+  return newArr;
+}
