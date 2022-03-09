5cd6c8b555084904aad900afe979a4613efd8e19
Janice Lee <meekukin@gmail.com>
2022-03-09 01:38:15 +0000

    working in progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,22 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+/*
+
+- two cards to each of four players and then declare the winner
+- create a collection of four objects that represent players. (name, hand)
+- create a collection of 52 objects that represent a standard 52 card deck (rank, suit)
+- shuffle the deck
+- deal two cards to each player(no two players should be able to receive the same card)
+- find the player with the highest score.
+- log the winning player to the console.
+
+*/
+
+// var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+// var courtCards = ['jack', 'queen', 'king', 'ace'];
+// var players = [
+//   {name: 'haku', hand: [], score: 0},
+//   {name: 'sen', hand: [], score: 0},
+//   {name: 'kiki', hand: [], score: 0},
+//   {name: 'toto', hand: [], score: 0}
+// ];
