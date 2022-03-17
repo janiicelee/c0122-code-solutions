24df160b50bcf5171af1c4c29800a89c51666c39
Janice Lee <meekukin@gmail.com>
2022-03-17 17:35:01 +0000

    Added comments

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
+// asynchronously reading the contents of the file
+// and encoding it using the utf8 character set
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+  if (err) {
+    // if there is an error
+    console.error(err); // write the error to the process's stderr
+    process.exit(1); // forcibly quit with a failure code(not 0) & not necessary
+  }
+  console.log(data); // otherwise print the file content to stdout
 });
