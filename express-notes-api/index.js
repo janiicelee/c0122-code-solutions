9dd2b9a22852258582f9d35148a1875f9dd804cc
Janice Lee <meekukin@gmail.com>
2022-03-21 15:59:34 -0700

    Modified put method

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -79,7 +79,7 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.put('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
+  const id = Number(req.params.id);
 
   if (id < 0) {
     return res.status(400).json({ error: 'id must be a positive integer.' });
