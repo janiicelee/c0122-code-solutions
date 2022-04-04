7c0e1abfec33ead615552bafb1b58e514c10c185
Janice Lee <meekukin@gmail.com>
2022-04-04 18:57:48 +0000

    Added createStaticMiddleware example

--- express-static/index.js
+++ express-static/index.js
@@ -5,6 +5,24 @@ const app = express();
 const join = path.join(__dirname, 'public');
 // console.log(join);
 
+// function createStaticMiddleware(directoryPath) {
+
+//   return (req, res, next) => {
+//     const filepath = req.url;
+//     const absoluteFilePath = path.join(directoryPath, filepath);
+//     fs.readFile(absoluteFilePath, 'utf8', (err, fileContent)=> {
+//       if(err) {
+//         next(err);
+//       } else {
+//         res.send(fileContent);
+//       }
+//     });
+//     //find out what is being requested
+//     // try to read that file that's being requested
+//     // respond to the client with the file contents
+//   };
+// }
+
 const staticValue = express.static(join);
 // console.log(staticValue);
 
