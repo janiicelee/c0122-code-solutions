7c9a58bc09ede0d97d3312f53aa765461e480f18
Janice Lee <meekukin@gmail.com>
2022-04-09 00:11:58 +0000

    added comments to main.js

--- javascript-closures/main.js
+++ javascript-closures/main.js
@@ -3,6 +3,8 @@ function graduate(credential) {
     console.log(fullName + ', ' + credential);
   };
 }
+// es6 version
+// const grad = credential => fullName => `${fullName}, ${credential}`
 
 const medicalSchool = graduate('M.D.');
 const lawSchool = graduate('Esq.');
